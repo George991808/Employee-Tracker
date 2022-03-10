e2a2162e89b0f8e2217b8f34ee4712bc0c824110
George991808 <gellis7@gmail.com>
2022-03-10 15:23:04 +0800

    use faker to create departments in seed.js

--- database/seed/seed.js
+++ database/seed/seed.js
@@ -1,3 +1,13 @@
 const {createDepartment} = require("../../src/departments");
+const {faker} = require('@faker-js/faker')
 
-createDepartment('abc')
+async function seed() {
+    //seed departmnet
+        for (let index = 0; index <10; index++){
+            await createDepartment(faker.commerce.department());
+        }
+    //seed roles
+
+    //seed employees
+}
+seed();
